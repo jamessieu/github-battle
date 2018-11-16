c95ed72a5615c9a12ff23dd6f7010aa83a0cd257
James Sieu <jamessieu@Jamess-MacBook-Pro.local>
2018-11-16 15:09:48 -0800

    completed Battle component

--- app/components/App.js
+++ app/components/App.js
@@ -7,20 +7,24 @@ const Switch = ReactRouter.Switch;
 const Nav = require('./Nav');
 const Home = require('./Home');
 const Battle = require('./Battle');
+const Results = require('./Results');
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className='container'>
+        <div className="container">
           <Nav />
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/battle' component={Battle} />
-            <Route path='/popular' component={Popular} />
-            <Route render={function () {
-              return (<p>Not Found</p>);
-            }} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/battle/results" component={Results} />
+            <Route path="/popular" component={Popular} />
+            <Route
+              render={function() {
+                return <p>Not Found</p>;
+              }}
+            />
           </Switch>
         </div>
       </Router>
---
+++ app/components/Results.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+class Results extends React.Component {
+  render() {
+    return <div>Results</div>;
+  }
+}
+
+module.exports = Results;
